f22aba0cacf90ccf36358c2e1719568a807b3a88
dollyllama <edolly2@protonmail.com>
2021-12-29 01:48:21 -0600

    ""

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import AllPosts from "./components/AllPosts.js";
+import OnePost from "./components/OnePost.js";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route element={<AllPosts />} exact path="/" />
+          <Route element={<OnePost />} path="/:slug" />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
